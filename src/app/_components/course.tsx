f542a004b7b3e4eb4081fe0efc6dbada78034e61
Benson <Yingxuan.li1@uq.net.au>
2024-09-02 00:11:55 +1000

    ui prototype

--- src/app/_components/course.tsx
+++ src/app/_components/course.tsx
@@ -2,8 +2,18 @@
 
 import { AssessmentDetail } from "~/server/utils/courseScraper";
 import { api } from "~/trpc/react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 
 export function Course() {
+  //
   const [course] = api.course.getCourseById.useSuspenseQuery({
     courseId: "63693c8f-ba05-4521-b9b1-8535bf1594c1",
   });
@@ -14,8 +24,24 @@ export function Course() {
   return (
     <>
       <h1 className="w-full max-w-xs">Course Info</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            {course?.courseCode} {course?.courseName}
+          </CardTitle>
+          <CardDescription>{course?.description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
       <div>{course?.courseCode}</div>
       <div>{course?.courseName}</div>
+      {!!course?.courseCode && <Button>{course?.courseCode}</Button>}
       <div>
         {assessments[0]?.title} : {assessments[0]?.weight}
       </div>
