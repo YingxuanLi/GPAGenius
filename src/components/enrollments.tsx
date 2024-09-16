3cb528212c008e0f397387eb9d4412e507c42218
Benson <Yingxuan.li1@uq.net.au>
2024-09-16 23:21:22 +1000

    add button to delete enrollment

--- src/components/enrollments.tsx
+++ src/components/enrollments.tsx
@@ -12,25 +12,32 @@ import {
 import { Input } from "~/components/ui/input";
 import { SearchCourse } from "./search-course";
 import { useEnrollmentStore } from "~/app/stores/enrollment-store";
-
+import { Button } from "./ui/button";
+import { CrossIcon } from "./icons/cross-icon";
 
 export function Enrollments() {
   // const userId = "9f632171-cc4b-4210-9b72-d5466923023b";
   const [fetchTrigger, setFetchTrigger] = useState(false);
-  const { enrollments, setEnrollments, setScore } =
-    useEnrollmentStore()
-  const { data: enrollmentsData, isLoading } =
-    api.user.getUserEnrollments.useQuery({ userId: "" });
+  const { enrollments, setEnrollments, setScore } = useEnrollmentStore();
+  const {
+    data: enrollmentsData,
+    isLoading,
+    refetch: refetchEnrollments,
+  } = api.user.getUserEnrollments.useQuery({ userId: "" });
 
   const updateAssessment = api.user.updateUserAssessment.useMutation({
     onSuccess: async (course) => {
-      console.info(
-        `assessment updated::${JSON.stringify(course)}`,
-      );
+      console.info(`assessment updated::${JSON.stringify(course)}`);
+    },
+  });
+
+  const deleteEnrollment = api.user.deleteUserEnrollment.useMutation({
+    onSuccess: async (enrollment) => {
+      console.info(`successfully deleted::${JSON.stringify(enrollment)}`);
+      refetchEnrollments();
     },
   });
   type ArrayElement<T> = T extends (infer U)[] ? U : null;
-
 
   useEffect(() => {
     if (enrollmentsData) {
@@ -53,9 +60,13 @@ export function Enrollments() {
     // setScores(Number(score));
   };
 
+  const handleEnrollmentDelete = (enrollmentId: string) => {
+    deleteEnrollment.mutate({ enrollmentId });
+  };
+
   const calculateTotalScore = (assessments: any[]) => {
     return assessments.reduce((total, assessment, index) => {
-      return total + (assessment.mark * parseFloat(assessment.weight));
+      return total + assessment.mark * parseFloat(assessment.weight);
     }, 0);
   };
 
@@ -64,56 +75,68 @@ export function Enrollments() {
       <h1 className="mb-6 text-3xl font-bold">My Course Enrollments</h1>
       <SearchCourse />
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {enrollments?.map((enrollment: ArrayElement<typeof enrollmentsData>) => (
-          <Card key={enrollment?.id} className="h-full">
-            <CardHeader>
-              <CardTitle className="truncate whitespace-normal text-xl sm:text-2xl md:text-3xl lg:text-2xl xl:text-3xl">
-                {enrollment?.course.courseCode} - {enrollment?.course.courseName}
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <div className="space-y-4">
-                {enrollment?.assessments.map((assessment) => (
-                  <div
-                    key={assessment.id}
-                    className="flex items-center justify-between"
+        {enrollments?.map(
+          (enrollment: ArrayElement<typeof enrollmentsData>) => (
+            <Card key={enrollment?.id} className="h-full">
+              <CardHeader>
+                <div className="flex items-center justify-between">
+                  <CardTitle className="truncate whitespace-normal text-xl sm:text-2xl md:text-3xl lg:text-2xl xl:text-3xl">
+                    {enrollment?.course.courseCode} -{" "}
+                    {enrollment?.course.courseName}
+                  </CardTitle>
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => handleEnrollmentDelete(enrollment?.id || '')}
                   >
-                    <div>
-                      <div className="font-medium">
-                        {assessment.assignmentName}
-                      </div>
-                      <div className="text-sm text-muted-foreground">
-                        Weight: {assessment.weight * 100}%
-                      </div>
-                    </div>
-                    <Input
-                      type="string"
-                      min="0"
-                      max="100"
-                      value={assessment.mark || 0}
-                      onChange={(e) =>
-                        handleScoreChange(
-                          enrollment.id,
-                          assessment.id,
-                          Number(e.target.value),
-                        )
-                      }
-                      className="w-20"
-                    />
-                  </div>
-                ))}
-              </div>
-            </CardContent>
-            <CardFooter>
-              <div className="flex items-center justify-between">
-                <div className="font-medium">Overall Grade:</div>
-                <div className="text-2xl font-bold">
-                  {calculateTotalScore(enrollment!.assessments).toFixed(2)}
+                    <CrossIcon className="h-4 w-4 hover:text-red-500" />
+                  </Button>
                 </div>
-              </div>
-            </CardFooter>
-          </Card>
-        ))}
+              </CardHeader>
+              <CardContent>
+                <div className="space-y-4">
+                  {enrollment?.assessments.map((assessment) => (
+                    <div
+                      key={assessment.id}
+                      className="flex items-center justify-between"
+                    >
+                      <div>
+                        <div className="font-medium">
+                          {assessment.assignmentName}
+                        </div>
+                        <div className="text-sm text-muted-foreground">
+                          Weight: {assessment.weight * 100}%
+                        </div>
+                      </div>
+                      <Input
+                        type="string"
+                        min="0"
+                        max="100"
+                        value={assessment.mark || 0}
+                        onChange={(e) =>
+                          handleScoreChange(
+                            enrollment.id,
+                            assessment.id,
+                            Number(e.target.value),
+                          )
+                        }
+                        className="w-20"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </CardContent>
+              <CardFooter>
+                <div className="flex items-center justify-between">
+                  <div className="font-medium">Overall Grade:</div>
+                  <div className="text-2xl font-bold">
+                    {calculateTotalScore(enrollment!.assessments).toFixed(2)}
+                  </div>
+                </div>
+              </CardFooter>
+            </Card>
+          ),
+        )}
       </div>
     </div>
   );
